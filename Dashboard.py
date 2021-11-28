1a2f9d418766bb15d54c190c9261607469035ac1
elat819 <83850638+elat819@users.noreply.github.com>
2021-11-27 22:54:09 -0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 import os
-from streamlit_folium import folium_static
+#from streamlit_folium import folium_static
 import folium
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
